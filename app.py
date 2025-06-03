937d6842aae7d07a4d967b8aa2e88b7348c0d9b1
AlexDev <alexnunes.patos@gmail.com>
2025-06-03 15:35:02 -0300

    teste de modificações

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Site Projetos em Flask funcionando!</h1>"
+    return "<h1>Meu novo site em flask</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8086)
